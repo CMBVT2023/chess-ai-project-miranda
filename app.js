c25ddcc7b417716914788ee169fa41bbaf6bab75
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-08-20 12:43:19 -0700

    Initialized the package.json to allow for node_moduels to be install directly in the project directory. Also, implemented the chessjs api to allow for the chess board to be playable.

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-let mainBoard = Chessboard('mainBoard', 'start')
+import { Chess } from "./node_modules/chess.js/dist/esm/chess.js"
+
+function randomMove() {
+    let possibleMoves = newGame.moves();
+
+    if (newGame.isGameOver()) {
+        if (newGame.isCheckmate()) {
+            console.log(newGame.turn() + " is the loser.")
+        } else if (newGame.isStalemate() || newGame.isDraw()) {
+            let reason = newGame.isStalemate() ? 'Stalemate' : 'Draw' 
+            console.log("This one is a", reason)
+        }
+        return
+    }
+
+    let randomIdx = Math.floor(Math.random() * possibleMoves.length);
+    newGame.move(possibleMoves[randomIdx]);
+    mainBoard.position(newGame.fen())
+
+    setTimeout(randomMove, 1);
+};
+
+let mainBoard = Chessboard('mainBoard', 'start');
+const newGame = new Chess();
+
+setTimeout(randomMove, 1)
